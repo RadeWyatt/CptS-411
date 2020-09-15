ca4c4881dbf5068b680cfe635abb791d2fbe9d51
Rade <rade.pilkinton@wsu.edu>
2020-09-15 16:44:52 -0700

    yeah

--- send_recv_test.c
+++ send_recv_test.c
@@ -36,7 +36,7 @@ int main(int argc,char *argv[])
 		x = (char *)malloc(pow(2, i) * sizeof(char));
 		int dest = 0;
 		gettimeofday(&t1,NULL);
-		MPI_Send(x,i,MPI_INT,dest,0,MPI_COMM_WORLD);
+		MPI_Send(x,pow(2,i),MPI_CHAR,dest,i,MPI_COMM_WORLD);
 		gettimeofday(&t2,NULL);
 		int tSend = (t2.tv_sec-t1.tv_sec)*1000 + (t2.tv_usec-t1.tv_usec)/1000;
 
@@ -44,11 +44,11 @@ int main(int argc,char *argv[])
 	   }
    } else 
    if (rank==0) {
-	   for (int i = 1; i < 10; i++)
+	   for (int i = 0; i < 10; i++)
 	   {
 		MPI_Status status;
 		gettimeofday(&t1,NULL);
-   		MPI_Recv(y,i,MPI_INT,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
+   		MPI_Recv(y,pow(2,i),MPI_CHAR,MPI_ANY_SOURCE,i,MPI_COMM_WORLD,&status);
 		gettimeofday(&t2,NULL);
 		int tRecv = (t2.tv_sec-t1.tv_sec)*1000 + (t2.tv_usec-t1.tv_usec)/1000;
 		printf("Rank=%d: received message %ld from rank %d; Recv time %d millisec\n",rank, sizeof(y) ,status.MPI_SOURCE,tRecv);
