6f8db8fa6ddd108a735340c8e8d9c52057b8568d
Rade <rade.pilkinton@wsu.edu>
2020-09-15 18:49:08 -0700

    test

--- send_recv_test.c
+++ send_recv_test.c
@@ -43,7 +43,7 @@ int main(int argc,char *argv[])
 
 	assert(p>=2);
 
-	for(int numInts = 10; numInts < 100000; numInts*=10)
+	for(int numInts = 10; numInts < 10000000; numInts*=10)
 	{
 		if(rank==1) {
 			msgSize = sizeof(int) * numInts;
@@ -55,9 +55,10 @@ int main(int argc,char *argv[])
 			int tSend = (t2.tv_sec-t1.tv_sec)*1000 + (t2.tv_usec-t1.tv_usec)/1000;
 			printf("Rank=%d: sent message to rank %d; size: %d bytes; Send time %d millisec\n", rank,dest,msgSize,tSend);
 		} else if (rank==0) {
+			MPI_Status status;
 			y = (int *)malloc(sizeof(int) * numInts);
 			gettimeofday(&t1,NULL);
-			MPI_Recv(y,numInts,MPI_INT,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(y,numInts,MPI_INT,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
 			gettimeofday(&t2,NULL);
 			int tRecv = (t2.tv_sec-t1.tv_sec)*1000 + (t2.tv_usec-t1.tv_usec)/1000;
 			printf("Rank=%d: received message %d from rank %d; Recv time %d millisec\n",rank, y[0],status.MPI_SOURCE,tRecv);
