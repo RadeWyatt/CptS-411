7ca1fb90dc28f165dfee99d8520b6d5d79e851be
Pilkinton <rade.pilkinton@pleiades.tricity.wsu.edu>
2020-09-16 18:53:34 -0700

    trying to add nonblocking calls

--- send_recv_test.c
+++ send_recv_test.c
@@ -52,7 +52,7 @@ int main(int argc,char *argv[])
 	int *y, *x;
 
 	int blockSendTime[numIterations], blockRecTime[numIterations];
-	//int nonBlockSendTime[numIterations], nonBlockRecTime[numIterations];
+	int nonBlockSendTime[numIterations], nonBlockRecTime[numIterations];
 
 	MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
@@ -87,35 +87,36 @@ int main(int argc,char *argv[])
 		}
 	}
 
-	/*
+	printArray(blockSendTime, blockRecTime, rank);
+
 	for(int j = 0; j < numIterations; j++)
 	{
 		int numInts = power(2, j);
 		int msgSize = sizeof(int) * numInts;
 		if(rank==1) {
-			x = (int *)calloc(numInts, sizeof(int));
-			int dest = 0;
-			gettimeofday(&t1,NULL);
-			MPI_Send(x,numInts,MPI_INT,dest,0,MPI_COMM_WORLD);
-			gettimeofday(&t2,NULL);
-			int sendMicro = (t2.tv_sec-t1.tv_sec)*1000000 + (t2.tv_usec-t1.tv_usec);
-			nonBlockSendTime[j] = sendMicro;
+			for (int k = 0; k < 10; k++) {
+				x = (int *)calloc(numInts, sizeof(int));
+				int dest = 0;
+				gettimeofday(&t1,NULL);
+				MPI_Send(x,numInts,MPI_INT,dest,0,MPI_COMM_WORLD);
+				gettimeofday(&t2,NULL);
+				int sendMicro = (t2.tv_sec-t1.tv_sec)*1000000 + (t2.tv_usec-t1.tv_usec);
+				nonBlockSendTime[j] = (nonBlockSendTime[j] + sendMicro) / 2;
+			}
 		} else if (rank==0) {
 			MPI_Request request;
 			MPI_Status status;
-			y = (int *)malloc(msgSize);
-			gettimeofday(&t1,NULL);
-			MPI_Irecv(y,numInts,MPI_INT,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&request);
-			MPI_Wait(&request, &status);
-			gettimeofday(&t2,NULL);
-			int recMicro = (t2.tv_sec-t1.tv_sec)*1000000 + (t2.tv_usec-t1.tv_usec);
-			nonBlockRecTime[j] = recMicro;
+			for (int k = 0; k < 10; k++) {
+				y = (int *)calloc(numInts, sizeof(int));
+				gettimeofday(&t1,NULL);
+				MPI_Irecv(y,numInts,MPI_INT,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&request);
+				MPI_Wait(&request, &status);
+				gettimeofday(&t2,NULL);
+				int recMicro = (t2.tv_sec-t1.tv_sec)*1000000 + (t2.tv_usec-t1.tv_usec);
+				nonBlockRecTime[j] = (nonBlockRecTime[j] + recMicro) / 2;
+			}
 		}
 	}
-	*/
-
+	printArray(nonBlockSendTime, nonBlockRecTime, rank);
 	MPI_Finalize();
-
-	printArray(blockSendTime, blockRecTime, rank);
-	//printArray(nonBlockSendTime, nonBlockRecTime, rank);
 }
